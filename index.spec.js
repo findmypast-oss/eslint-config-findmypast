96781470e6a53bdae9578138baa58eb21855217b
Neil Crawford <neil.crawford@gmail.com>
2017-08-23 12:27:26 +0100

    PRO233 adding some more rules

---
+++ index.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const assert = require('assert');
+const eslint = require('eslint');
+const conf = require('.');
+
+// The source files to lint.
+const repoFiles = ['index.js', 'index.spec.js', 'react.js'];
+
+// Use the rules defined in this repo to test against.
+const eslintOpts = {
+  useEslintrc: false,
+  envs: ['node', 'es6'],
+  parserOptions: { ecmaVersion: 2017 },
+  rules: conf.rules,
+};
+
+// Runs the linter on the repo files and asserts no errors were found.
+const report = new eslint.CLIEngine(eslintOpts).executeOnFiles(repoFiles);
+assert.equal(report.errorCount, 0);
+assert.equal(report.warningCount, 0);
+repoFiles.forEach((file, index) => {
+  assert(report.results[index].filePath.endsWith(file));
+});
---
+++ react.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'prettier/react'],
+  plugins: ['react'],
+  rules: {
+    'react/no-danger': 2,
+    'react/prop-types': 0,
+    'react/jsx-no-bind': 2,
+    'react/prefer-stateless-function': 2,
+  },
+};
