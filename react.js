743f820a4f3dcca75991a02b95c042e5ce24e304
Stephen Cassels <scassels@findmypast.com>
2018-05-25 13:03:45 +0100

    CHP000: :lock: make `target=_blank` secure.

--- react.js
+++ react.js
@@ -1,10 +1,11 @@
 module.exports = {
-  extends: ['plugin:react/recommended', 'prettier/react'],
-  plugins: ['react'],
+  extends: ["plugin:react/recommended", "prettier/react"],
+  plugins: ["react"],
   rules: {
-    'react/no-danger': 2,
-    'react/prop-types': 0,
-    'react/jsx-no-bind': 2,
-    'react/prefer-stateless-function': 2,
-  },
+    "react/no-danger": 2,
+    "react/prop-types": 0,
+    "react/jsx-no-bind": 2,
+    "react/prefer-stateless-function": 2,
+    "react/jsx-no-target-blank": 2
+  }
 };
